6d06a35f11d2f5e83d80f09de51d7c1322188a72
moogar0880 <moogar0880@gmail.com>
2016-04-03 14:09:02 -0400

    fields are now properly inherited. added custom array field unpacking options

--- pystructs/models.py
+++ pystructs/models.py
@@ -3,8 +3,8 @@
 import logging
 import struct
 from collections import OrderedDict, Iterable
-from pystructs.fields import StructFieldDescriptor
-from pystructs.utils import StatefulByteStream
+from pystructs.fields import StructFieldDescriptor, ArrayField
+from pystructs.utils import StatefulByteStream, safe_decode
 
 NativeByteOrder = '@'
 StandardNativeByteOrder = '='
@@ -32,8 +32,17 @@ class StructObjectMeta(type):
         """
         fields = [key for key, val in clsdict.items()
                   if isinstance(val, StructFieldDescriptor)]
+
+        # enable inheriting our parent class's struct fields, if they're present
+        for base in bases:
+            if issubclass(base, StructFieldDescriptor) and hasattr(base, '_fields'):
+                for field in base._fields:
+                    field_obj = base.__dict__[field]
+                    clsdict[field_obj._name] = field_obj
+                fields.extend(base._fields)
+
         for name in fields:
-            clsdict[name].name = name
+            clsdict[name]._name = name
         clsdict['_fields'] = fields
 
         return super().__new__(cls, clsname, bases, dict(clsdict))
@@ -45,22 +54,26 @@ class StructObject(StructFieldDescriptor, metaclass=StructObjectMeta):
     #: :const:`NetworkByteOrder`
     BYTE_ORDER = NetworkByteOrder
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = logging.getLogger(self.__class__.__name__)
+
     @property
     def __fields(self):
         """Dynamic accessors which returns access to all field descriptors"""
         return [self.__class__.__dict__[f] for f in self.__class__._fields]
 
     @property
-    def size(self):
-        return sum(f.size for f in self.__fields)
+    def _size(self):
+        return sum(f._size for f in self.__fields)
 
     @property
-    def ctype(self):
+    def _ctype(self):
         return self.__class__.__qualname__
 
     @property
-    def format(self):
-        return ''.join(f.format for f in self.__fields)
+    def _format(self):
+        return ''.join(f._format for f in self.__fields)
 
     def _unpack_sizeof(self, field, stream):
         """handle unpacking a :class:`StructFieldDescriptor` that has a sizeof
@@ -69,15 +82,38 @@ class StructObject(StructFieldDescriptor, metaclass=StructObjectMeta):
         :param field: The :class:`StructFieldDescriptor` to unpack
         :param stream: A :const:`bytes` object to extract data from
         """
-        field.count = field.sizeof(self)
-        field.val = struct.unpack(
-            self.BYTE_ORDER + field.format,
-            stream.slice(field.size*field.count)
+        field._count = field._sizeof(self)
+        self.logger.info('unpacking sizeof field %s (%s) as %s', str(field._name), str(field), field._format)
+        field._val = struct.unpack(
+            self.BYTE_ORDER + field._format,
+            stream.slice(field._size*field._count)
         )
         # for char[] types, unless the encoding is set to None, decode
-        # the field.val tuple using the specified encoding
-        if field.typ is bytes and field.encoding is not None:
-            field.val = field.val[0].decode(field.encoding)
+        # the field._val tuple using the specified encoding
+        if field._typ is bytes and field._encoding is not None:
+            field._val = safe_decode(field._val[0], field._encoding)
+
+    def _unpack_array(self, field, stream):
+        _count = field._count(self)
+        _size = int(field._sizeof(self) / _count)
+        field._val = [
+            struct.unpack(self.BYTE_ORDER + '%d%s' % (_size, field._fmt),
+                          stream.slice(_size))
+            for _ in range(_count)
+        ]
+
+        typed_values = []
+        for f in field._val:
+            base = field._base_field(**field._kwargs)
+            base._val = f
+            if base._typ is bytes and base._encoding is not None:
+                base._val = [safe_decode(val, base._encoding)
+                             for val in base._val]
+            else:
+                field._val = [val for val in base._val]
+            typed_values.extend(base._val)
+        field._val = typed_values
+
 
     def unpack(self, stream):
         """unpack the provided binary data stream into this class's fields
@@ -88,26 +124,30 @@ class StructObject(StructFieldDescriptor, metaclass=StructObjectMeta):
         """
         if not isinstance(stream, StatefulByteStream):
             stream = StatefulByteStream(stream)
+            self.logger.info('unpacking stream of size %d', len(stream))
 
         # map unpacked values to the fields that they correspond to
         for field in self.__fields:
-            fmt = (field.byte_order or self.BYTE_ORDER) + field.format
-            slice_size = field.size * field.count
+            # self.logger.info('unpacking field %s (%s) as %s', str(field._name), str(field), field._format)
             if isinstance(field, StructObject):
-                field.val = field.unpack(stream.slice(slice_size))
-            elif field.sizeof is not None:
+                field._val, stream = field.unpack(stream)
+            elif isinstance(field, ArrayField):
+                self._unpack_array(field, stream)
+            elif field._sizeof is not None:
                 self._unpack_sizeof(field, stream)
             else:
-                field.val = struct.unpack(fmt, stream.slice(slice_size))
+                fmt = (field._byte_order or self.BYTE_ORDER) + field._format
+                slice_size = field._size * field._count
+                field._val = struct.unpack(fmt, stream.slice(slice_size))
 
             # if we unpacked something that was not a collection or a byte
             # string, remove the single element from it's containing tuple
-            if field.count == 1 and field.typ is not bytes and isinstance(field.val, tuple):
-                field.val = field.val[0]
+            if field._count == 1 and field._typ is not bytes and isinstance(field._val, tuple):
+                field._val = field._val[0]
 
         # return the current instance from unpack to facilitate chaining
         # :class:`StructObject` instances
-        return self
+        return self, stream
 
     def pack(self):
         """Return this class as a :const:`bytes` representation of this
@@ -118,24 +158,24 @@ class StructObject(StructFieldDescriptor, metaclass=StructObjectMeta):
         """
         output = b''
         for field in self.__fields:
-            if isinstance(field.val, StructObject):
+            if isinstance(field._val, StructObject):
                 output += struct.pack(
-                    (field.byte_order or self.BYTE_ORDER) + field.format,
-                    *[f.val for f in field.__fields]
+                    (field._byte_order or self.BYTE_ORDER) + field._format,
+                    *[f._val for f in field.__fields]
                 )
-            elif isinstance(field.val, str):
+            elif isinstance(field._val, str):
                 output += struct.pack(
-                    (field.byte_order or self.BYTE_ORDER) + field.format,
-                    bytes(field.val, field.encoding or 'utf-8')
+                    (field._byte_order or self.BYTE_ORDER) + field._format,
+                    bytes(field._val, field._encoding or 'utf-8')
                 )
-            elif isinstance(field.val, Iterable) and not isinstance(field.val, bytes):
+            elif isinstance(field._val, Iterable) and not isinstance(field._val, bytes):
                 output += struct.pack(
-                    (field.byte_order or self.BYTE_ORDER) + field.format,
-                    *field.val
+                    (field._byte_order or self.BYTE_ORDER) + field._format,
+                    *field._val
                 )
             else:
                 output += struct.pack(
-                    (field.byte_order or self.BYTE_ORDER) + field.format,
-                    field.val
+                    (field._byte_order or self.BYTE_ORDER) + field._format,
+                    field._val
                 )
         return output
